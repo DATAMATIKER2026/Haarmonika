508c767a171cf41b482350c4d7b46624bde9edb8
Tobiasrolesen <tobiasrolesen@gmail.com>
2026-03-02 13:52:46 +0100

    Finished SceneNavigator Finished create customer

---
+++ Monika/src/main/java/org/example/monika/service/FrontPageService.java
@@ -0,0 +1,13 @@
+/*package org.example.monika.service;
+
+import org.example.monika.Repository.CustomerRepository;
+
+public class FrontPageService {
+
+    private final CustomerRepository customerRepo;
+
+    public FrontPageService(CustomerRepository customerRepo) {}
+
+}
+
+ */
